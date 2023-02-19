de3c42e428de8486f814fcb49fe8bcd9196a6bab
Huzaifa Bin Shahid <shuzaifa222@gmail.com>
2023-02-19 12:06:56 +0500

    initial commit

---
+++ src/Components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae unde
+        itaque sequi, expedita, inventore possimus illum officia eveniet
+        doloribus fuga nesciunt ipsum magnam voluptas sunt ad ea voluptates?
+        Laborum asperiores tempore dolore commodi labore!
+      </p>
+    </div>
+  );
+};
+
+export default About;
