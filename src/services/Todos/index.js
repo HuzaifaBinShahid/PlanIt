5108ace1289566fcbb3e02f80a787afaf39a0af3
Huzaifa <shuzafa222@gmail.com>
2025-04-09 19:17:18 +0500

    fix: unsued key element removed

--- src/services/Todos/index.js
+++ src/services/Todos/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const fetchTodos = async ({ queryKey }) => {
-  const [_key, searchText] = queryKey;
+  const [, searchText] = queryKey;
   const { data } = await axios.get(
     `http://localhost:5000/api/todos?searchKey=${searchText || ""}`
   );
